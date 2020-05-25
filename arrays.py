bd545db771ec4ab859c9bc5a5237688a14f0ea00
cchadha2 <chadhac@tcd.ie>
2020-05-25 09:13:36 +0100

    Adding CyclicRotation solution

--- arrays.py
+++ arrays.py
@@ -1,5 +1,14 @@
 from collections import Counter
 
+# CyclicRotation
+def solution(A, K):
+    if not A:
+        return A
+    for _ in range(K):
+        A.insert(0, A.pop())
+    return A
+
+# OddOccurerencesInArray 
 def solution(A):
     for x, occ in Counter(A).items():
         if occ % 2:
