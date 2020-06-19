e7e7a336c46db948bf4a64b6f5a8483a546b09fa
cchadha2 <chadhac@tcd.ie>
2020-06-19 20:28:24 +0100

    Finishing stacks and queues

--- stacks_and_queues.py
+++ stacks_and_queues.py
@@ -73,8 +73,56 @@ print(solution(A, B))
 A = [5, 4, 1, 1, 2, 8, 5]
 B = [0, 1, 1, 1, 0, 1, 0]
 print(solution(A, B))
+
 # Nesting
+def solution(S):
+    if not S:
+        return 1
+    stack = []
+    brackets = {"(": ")"}
+    for elem in S:
+        if elem in brackets:
+            stack.append(elem)
+        if elem in brackets.values():
+            if stack:
+                if elem == brackets[stack.pop()]:
+                    continue
+                else:
+                    return 0
+            else:
+                return 0
+    return 0 if stack else 1
 
+S = "(()(())())"
+print(solution(S))
 
+S = "())"
+print(solution(S))
 
 # StoneWall
+def solution(H):
+    stack = []
+    num_blocks = 0
+
+    for height in H:
+        while stack and stack[-1] > height:
+            stack.pop()
+        if stack and stack[-1] == height:
+            continue
+        else:
+            stack.append(height)
+            num_blocks += 1
+    return num_blocks
+
+H = [8, 8, 5, 7, 9, 8, 7, 4, 8]
+print(solution(H))
+
+
+H = [2, 3, 2, 1]
+print(solution(H))
+
+H = [1, 2, 3, 3, 2, 1]
+print(solution(H))
+
+H = [1, 1, 1]
+print(solution(H))
