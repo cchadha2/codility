b12180d16c487c07b02d19d23ce09791a5452b33
cchadha2 <chadhac@tcd.ie>
2020-06-19 19:00:35 +0100

    Adding Fish solution

--- stacks_and_queues.py
+++ stacks_and_queues.py
@@ -1,3 +1,4 @@
+# Brackets
 def solution(S):
     if not S:
         return 1
@@ -27,3 +28,53 @@ print(solution(S))
 
 S = "]]}}))"
 print(solution(S))
+
+# Fish
+def solution(A, B):
+    alive = [0]
+    for fish, size in enumerate(A[1:], 1):
+        print(f"fish currently alive: {alive}")
+        direction = B[fish]
+        while alive:
+            prev_size, prev_direction = A[alive[-1]], B[alive[-1]]
+            if prev_direction > direction:
+                if A[fish] < prev_size:
+                    # Get eaten by previous fish.
+                    print(f"{fish} is being eaten by {alive[-1]}")
+                    break
+                else:
+                    # Eat the previous fish.
+                    dead = alive.pop()
+                    print(f"{fish} is eating {dead}")
+                    continue
+            if prev_direction <= direction:
+                print(f"{fish} does not meet {alive[-1]}")
+                alive.append(fish)
+                # prev_size, prev_direction = size, direction
+                break
+        else:
+            alive.append(fish)
+    print(f"fish left alive: {alive}")
+    return len(alive)
+
+
+A = [4, 3, 2, 1, 5]
+B = [0, 1, 0, 0, 0]
+print(solution(A, B))
+
+A = [4, 3, 2, 1, 5]
+B = [1, 0, 0, 0, 0]
+print(solution(A, B))
+
+A = [0, 1]
+B = [1, 1]
+print(solution(A, B))
+
+A = [5, 4, 1, 1, 2, 8, 5]
+B = [0, 1, 1, 1, 0, 1, 0]
+print(solution(A, B))
+# Nesting
+
+
+
+# StoneWall
